ffb12e39346af8650cfd096d9f2b5b46a2b7001c
benjamin <2000beni770@gmail.com>
2024-07-15 17:20:55 +0300

    pass the pointer to extract func insted the valu

--- server/crawler/parser.cpp
+++ server/crawler/parser.cpp
@@ -11,10 +11,11 @@ class Parser
 public:
     Parser(std::queue<std::string>* url_queue);
     ~Parser();
-    void extract_urls(std::string str); //TODO pass a pointer of the string
+    void extract_urls(std::string* str); 
 private:
     std::queue<std::string>* m_url_queue;
 	void cut_link(std::string* str);
+	// void extract_words(std::string str);
 
 
 };
@@ -38,7 +39,7 @@ void Parser::cut_link(std::string* str)
 	*str = sub;
 }
 
-void Parser::extract_urls(std::string str)
+void Parser::extract_urls(std::string* str)
 {
 
 	// Regular Expression to extract URLs from the string
@@ -50,11 +51,11 @@ void Parser::extract_urls(std::string str)
 	std::regex r(regex_str, std::regex_constants::icase);
 
 	// Find the match between string and the regular expression
-	std::sregex_iterator m(str.begin(), str.end(), r);
+	std::sregex_iterator m(str->begin(), str->end(), r);
 	std::sregex_iterator m_end;
 
 	// find and store all the URLs in the queue
-	while (m != m_end) {
+	while (m != m_end) {	
 		std::string link = m->str();
 		this->cut_link(&link); //cut the sub string "<a href=" from the link
 		this->m_url_queue->push(link);
